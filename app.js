ee6b3a99f464be5599bf359e3441f5162fd50207
Garv Thakral <144711031+codebygarv@users.noreply.github.com>
2024-03-01 08:48:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-mongoose.connect('mongodb://localhost:27017/mongodb-form');
 
+const db =
+  "mongodb+srv://garvthakral90:garv1212@garv1.hifrubd.mongodb.net/";
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,8 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("Connected to db.");
+  })
+  .catch((error) => {
+    console.error("Error connecting to db.", error);
+  });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
